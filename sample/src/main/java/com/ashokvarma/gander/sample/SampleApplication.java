af1d5085ee6dd49d68b9c3cf6435778f1bf75350
Ashok Varma <ashok.v@uber.com>
2019-06-28 22:52:02 +0530

    3.0.1 room db is now exposed as api

--- sample/src/main/java/com/ashokvarma/gander/sample/SampleApplication.java
+++ sample/src/main/java/com/ashokvarma/gander/sample/SampleApplication.java
@@ -1,9 +1,10 @@
 package com.ashokvarma.gander.sample;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 import com.ashokvarma.gander.Gander;
-import com.ashokvarma.gander.imdb.GanderIMDB;
+import com.ashokvarma.gander.persistence.GanderPersistence;
 
 public class SampleApplication extends Application {
 
@@ -11,7 +12,19 @@ public class SampleApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-//        Gander.setGanderStorage(GanderPersistence.getInstance(this));
-        Gander.setGanderStorage(GanderIMDB.getInstance());
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .penaltyLog()
+                .build());
+
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .build());
+
+        Gander.setGanderStorage(GanderPersistence.getInstance(this));
+//        Gander.setGanderStorage(GanderIMDB.getInstance());
     }
 }
